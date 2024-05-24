c816ca75aec79005d8b6d142807fba33b411ab4c
Ana Paula Ribeiro <ana.sed26@hotmail.com>
2024-05-24 19:45:19 -0300

    Criando o projeto desafio-controle-de-fluxo

---
+++ desafio-controle-de-fluxo/src/Contador.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+              Scanner ler = new Scanner(System.in);
+		System.out.println("Digite o primeiro parâmetro: ");
+		int parametro1 = ler.nextInt();
+		System.out.println("Digite o segundo parâmetro: ");
+		int parametro2 = ler.nextInt();
+		
+		try {
+			contar(parametro1, parametro2);
+		
+		}catch (ParametrosInvalidosException exception) {
+			System.out.println(exception.getMessage()); 
+		}
+		
+	}
+	public static void contar(int parametro1, int parametro2) throws ParametrosInvalidosException {
+        if (parametro1 >= parametro2) {
+           throw new ParametrosInvalidosException();
+        }
+		
+		int contagem = parametro2 - parametro1;
+		for (int i = 0; i <= contagem; i++) {
+            System.out.println("Imprimindo o número: " + i);
+        }
+    }
+    
+}
